6a7581c09c3be1ea99d4b7b2840f8cec7fe892f5
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-11 13:37:30 -0700

    added empty project

---
+++ VideoPosts/VideoPosts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPosts
+//
+//  Created by Jorge Alvarez on 3/11/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
